cf2a14a02c8f886e153b3a6884b7208e7a865563
Ben-Hicks <bh.benhicks@gmail.com>
2018-06-24 17:26:45 -0400

    Movement + Blocks
    Initial commit.  Can move blocks and collide with walls.

---
+++ Assets/Moveable.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveable : MonoBehaviour {
+
+	const float fGravMag = 250.0f;
+
+	MovementHandler.Dir curDir;
+
+	Rigidbody rb;
+
+	public void SetGravity(MovementHandler.Dir _dir){
+
+		curDir = _dir;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		SetGravity (MovementHandler.Dir.None);
+	}
+
+	void ApplyForce(){
+		
+		Vector3 force = Vector3.zero;
+		switch (curDir) {
+		case MovementHandler.Dir.Down:
+			force = Vector3.down;
+			break;
+		case MovementHandler.Dir.Left:
+			force = Vector3.left;
+			break;
+		case MovementHandler.Dir.Right:
+			force = Vector3.right;
+			break;
+		case MovementHandler.Dir.Up:
+			force = Vector3.up;
+			break;
+		case MovementHandler.Dir.None:
+			break;
+		default:
+			Debug.LogError ("Unrecognized gravity direction");
+			break;
+		}
+		rb.AddForce (fGravMag * force * Time.deltaTime, ForceMode.Force);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ApplyForce ();
+
+
+	}
+}
---
+++ Assets/MovementHandler.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementHandler : MonoBehaviour {
+
+	public enum Dir {Up, Right, Down, Left, None};
+
+	public Dir curDir;
+
+	int [] listPressedDir = new int[4];
+	int nDirCount;
+
+	void PressDir(Dir _dir){
+		listPressedDir [(int)_dir] = nDirCount;
+		nDirCount++;
+		curDir = _dir;
+
+		foreach (Moveable m in Object.FindObjectsOfType<Moveable> ()) {
+			m.SetGravity (_dir);
+		}
+	}
+
+	void ReleaseDir(Dir _dir){
+		//When releasing, find the most recent timestamped
+		// direction pressed, and let that be the new current direction
+
+		listPressedDir [(int)_dir] = -1;
+		int nRecentPressed = -1;
+		for (int i = (int)Dir.Up; i <= (int)Dir.Left; i++) {
+			if (listPressedDir [i] > nRecentPressed) {
+				nRecentPressed = listPressedDir [i];
+				curDir = (Dir)i;
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		nDirCount = 0;
+
+		listPressedDir [(int)Dir.Up] = -1;
+		listPressedDir [(int)Dir.Right] = -1;
+		listPressedDir [(int)Dir.Down] = -1;
+		listPressedDir [(int)Dir.Left] = -1;
+
+		curDir = Dir.Down;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Detect releasing a direction key
+		if(Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.UpArrow)){
+			ReleaseDir(Dir.Up);
+		}
+
+		if(Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.RightArrow)){
+			ReleaseDir(Dir.Right);
+		}
+
+		if(Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.DownArrow)){
+			ReleaseDir(Dir.Down);
+		}
+
+		if(Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.LeftArrow)){
+			ReleaseDir(Dir.Left);
+		}
+			
+
+		//Detect pressing a direction key
+		if(Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow)){
+			PressDir(Dir.Up);
+		}
+
+		if(Input.GetKeyDown (KeyCode.D) || Input.GetKeyDown (KeyCode.RightArrow)){
+			PressDir(Dir.Right);
+		}
+
+		if(Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.DownArrow)){
+			PressDir(Dir.Down);
+		}
+
+		if(Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.LeftArrow)){
+			PressDir(Dir.Left);
+		}
+
+	}
+}
