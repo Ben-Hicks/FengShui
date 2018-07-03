2d552474a949c91fae05f12e738b730eddf5f33e
Ben-Hicks <bh.benhicks@gmail.com>
2018-07-03 00:19:58 -0400

    Basic Level Loading
    Enter -> next level

---
+++ Assets/GeneralManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralManager : MonoBehaviour {
+
+	LevelManager levelManager;
+
+	void Start(){
+		levelManager = LevelManager.Instance;
+		DontDestroyOnLoad (this);
+	}
+
+	void Update () {
+
+
+
+		if (Input.GetKeyUp (KeyCode.Return)) {
+			Debug.Log ("Next Level");
+			levelManager.NextLevel ();
+		} else if (Input.GetKeyUp (KeyCode.Backspace)) {
+			Debug.Log ("Previous Level");
+			levelManager.PreviousLevel ();
+		}
+
+	}
+}
---
+++ Assets/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : Singleton<LevelManager> {
+
+	public int nCurScene = 0;
+
+	public void NextLevel(){
+		nCurScene++;
+		GoToScene (nCurScene);
+	}
+
+	public void PreviousLevel(){
+		nCurScene--;
+		GoToScene (nCurScene);
+	}
+
+	void GoToScene(int nSceneID){
+		//if (nSceneID < SceneManager.sceneCount) {
+			Debug.Log ("Loading Level" + nSceneID);
+			SceneManager.LoadScene ("Level"+nSceneID, LoadSceneMode.Single);
+		//} else {
+		//	Debug.LogError ("No such level exists");
+		//}
+	}
+}
